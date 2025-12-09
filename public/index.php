7632f4eeca0eabdc2b2df1ebc3d5674ff93a539d
Vinícius de Senna <sennadevinicius@gmail.com>
2025-12-09 20:21:10 -0300

    Estrutura inicial do projeto

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<h1>Ambiente Docker Configurado!</h1>";
-echo "<p>Versão do PHP: " . phpversion() . "</p>";
-
-// Verifica se o composer está acessível via shell (opcional no script, mas bom para teste)
-$output = shell_exec('composer --version');
-echo "<pre>Composer: $output</pre>";
